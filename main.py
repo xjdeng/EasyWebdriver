a7a5bfeebf9d998a05ff35d531fc32b4f83e4818
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-02-03 16:36:43 -0600

    Remove EasyWebdriver object
    No longer needed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-from path import Path as path
-from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 
 try:
@@ -32,62 +30,3 @@ def Firefox(mypath = firefoxpath):
     else:
         return webdriver.Firefox()
     
-class EasyWebdriver(object):
-    
-    def __init__(self, webdriver_path = [], firefox_exe = ['firefox', \
-                 'Firefox'], chrome_exe = ['chrome', 'Chrome']):
-        self.webdriver_path = webdriver_path
-        if isinstance(firefox_exe, str):
-            self.firefox_exe = [firefox_exe]
-        else:
-            self.firefox_exe = firefox_exe
-        if isinstance(chrome_exe, str):
-            self.chrome_exe = [chrome_exe]
-        else:
-            self.chrome_exe = chrome_exe
-    
-    def add_chrome_exe(self, newexe):
-        self.chrome_exe.append(newexe)
-    
-    def add_firefox_exe(self, newexe):
-        self.firefox_exe.append(newexe)
-    
-    def add_webdriver_path(self, newpath):
-        self.webdriver_path.append(path(newpath))
-    
-    def _prep(self):
-        #https://stackoverflow.com/questions/1681208/python-platform-independent-way-to-modify-path-environment-variable
-        os.environ["PATH"] = old_environ + os.pathsep + \
-        os.pathsep.join(self.webdriver_path)
-    
-    def Chrome(self):
-        self._prep()
-        exp = Exception()
-        success = False
-        for c in self.chrome_exe:
-            try:
-                return webdriver.Chrome(c)
-                success = True
-                break
-            except Exception as exp:
-                pass
-        if success == False:
-            return webdriver.Chrome()
-        else:
-            raise(exp)
-        
-    def Firefox(self):
-        self._prep()
-        exp = Exception()
-        success = False
-        for f in self.firefox_exe:
-            try:
-                return webdriver.Firefox(f)
-                success = True
-                break
-            except Exception as exp:
-                pass
-        if success == False:
-            return webdriver.Firefox()
-        else:
-            raise(exp)
