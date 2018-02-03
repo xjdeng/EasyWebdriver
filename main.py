fe8ca02ac371c4ce80c778a799c5f55c43e3e741
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-02-03 16:35:35 -0600

    Add Webdriver for Firefox

--- main.py
+++ main.py
@@ -12,14 +12,25 @@ try:
     chromepath = os.environ['CHROME']
 except KeyError:
     chromepath = None
+
+try:
+    firefoxpath = os.environ['FIREFOX']
+except KeyError:
+    firefoxpath = None
     
-def Chrome():
+def Chrome(mypath = chromepath):
     if chromepath is not None:
         opts = webdriver.ChromeOptions()
-        opts.binary_location = path(chromepath)
+        opts.binary_location = chromepath
         return webdriver.Chrome(options = opts)
     else:
         return webdriver.Chrome()
+
+def Firefox(mypath = firefoxpath):
+    if firefoxpath is not None:
+        return webdriver.Firefox(firefox_binary = firefoxpath)
+    else:
+        return webdriver.Firefox()
     
 class EasyWebdriver(object):
     
