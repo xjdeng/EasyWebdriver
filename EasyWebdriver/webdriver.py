b7f21e311513bfadeeafbcf2ad9e67337ee9daac
xjdeng <xjd001@gmail.com>
2018-08-03 20:29:21 -0500

    Add browser and debug input variables to __init__

--- EasyWebdriver/webdriver.py
+++ EasyWebdriver/webdriver.py
@@ -9,7 +9,10 @@ from selenium import webdriver as oldwebdriver
     
 class webdriver(object):
     
-    def __init__(self, retry_delay = 0.1, retries = 100, final_delay = 0):
+    def __init__(self, browser, retry_delay = 0.1, retries = 100, \
+                 final_delay = 0, debug = True):
+        self.browser = browser
         self.retry_delay = retry_delay
         self.retries = retries
         self.final_delay = final_delay
+        self.debug = debug
