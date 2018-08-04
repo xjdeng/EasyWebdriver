685b5ceb4ee312a1a55c4d591de464518f89d255
xjdeng <xjd001@gmail.com>
2018-08-03 20:51:20 -0500

    Add run() method to run a webdriver method

--- EasyWebdriver/webdriver.py
+++ EasyWebdriver/webdriver.py
@@ -6,6 +6,15 @@ except ImportError:
     import main
 
 from selenium import webdriver as oldwebdriver
+from selenium.common.exceptions import ElementNotInteractableException, \
+StaleElementReferenceException, ElementClickInterceptedException, \
+NoSuchElementException, TimeoutException
+import time
+
+common_exceptions = (ElementNotInteractableException,\
+                     StaleElementReferenceException,\
+                     ElementClickInterceptedException,
+                     NoSuchElementException,TimeoutException)
     
 class webdriver(object):
     
@@ -16,3 +25,26 @@ class webdriver(object):
         self.retries = retries
         self.final_delay = final_delay
         self.debug = debug
+    
+    def run(self, methodstr, *args, **kwargs):
+        tries = 0
+        e = Exception()
+        while tries < self.retries:
+            try:
+                tmp = getattr(self.browser, methodstr)(*args, **kwargs)
+                if self.debug == True:
+                    print("Success! {}({}, {})").format(methodstr, args, kwargs)
+                time.sleep(self.final_delay)
+                return tmp
+            except common_exceptions as e:
+                if self.debug == True:
+                    print("Exception! {}({}, {}) failed with {}".format(methodstr,\
+                          args, kwargs, e))
+                time.sleep(self.retry_delay)
+                tries += 1
+        if self.debug == True:
+            print("Failed! {}({}, {}) failed with {}".format(methodstr, args,\
+                  kwargs, e))
+        raise(e)
+                
+        
